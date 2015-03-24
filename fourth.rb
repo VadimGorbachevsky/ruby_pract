7a0f0c32bb802cd01b931e826b7e452d2f212ac3
VadimGorbachevsky <shaersorrel@gmail.com>
2015-03-24 12:38:40 +0500

    Четвертое доделано

--- fourth.rb
+++ fourth.rb
@@ -1,22 +1,37 @@
-@NumbersCalcView={
-1 => ['   ','  |','  |'],
-2 => [' _ ',' _|','|_ '],
-3 => [' _ ',' _|',' _|'],
-4 => ['   ','|_|','  |'],
-5 => [' _ ','|_ ',' _|'],
-6 => [' _ ','|_ ','|_|'],
-7 => [' _ ','  |','  |'],
-8 => [' _ ','|_|','|_|'],
-9 => [' _ ','|_|',' _|'],
-0 => [' _ ','| |','|_|']
-}
-@Output = []
-@InputNumber = 133563413290
-@Buffer = @InputNumber
-while(@Buffer>0)
-     @Output << @NumbersCalcView[@Buffer%10]
-     @Buffer = @Buffer / 10;
+def OutputInLCDView(input_number)
+          
+          @NumbersCalcView = {
+          1 => ['   ','  |','  |'],
+          2 => [' _ ',' _|','|_ '],
+          3 => [' _ ',' _|',' _|'],
+          4 => ['   ','|_|','  |'],
+          5 => [' _ ','|_ ',' _|'],
+          6 => [' _ ','|_ ','|_|'],
+          7 => [' _ ','  |','  |'],
+          8 => [' _ ','|_|','|_|'],
+          9 => [' _ ','|_|',' _|'],
+          0 => [' _ ','| |','|_|']
+          }
+          
+          @Buffer = input_number
+          
+          def CreateOutArray()
+               @Output = []
+               while(@Buffer>0)
+                    @Output << @NumbersCalcView[@Buffer%10]
+                    @Buffer = @Buffer / 10;
+               end
+          end
+
+          def TransposeOut()
+               puts(@Output.reverse.transpose[0].join(" "))
+               puts(@Output.reverse.transpose[1].join(" "))
+               puts(@Output.reverse.transpose[2].join(" "))
+          end
+     #body
+     CreateOutArray()
+     TransposeOut()
 end
-puts(@Output.reverse.transpose[0].join(" "))
-puts(@Output.reverse.transpose[1].join(" "))
-puts(@Output.reverse.transpose[2].join(" "))
+
+@InputNumber = ARGV[0].to_i
+OutputInLCDView(@InputNumber)

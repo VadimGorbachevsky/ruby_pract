41cc52f812acc75afdceda1e1ca46b78fd12b917
VadimGorbachevsky <shaersorrel@gmail.com>
2015-03-26 09:18:26 +0500

    Корректирование области видимости переменных в задании 5.

--- fifth.rb
+++ fifth.rb
@@ -1,16 +1,16 @@
 def first_usefull_st(input_file, trash_identity_string)
-  @string = input_file.gets
-  @string = input_file.gets while @string.include?(trash_identity_string)
-  return @string
+  string = input_file.gets
+  string = input_file.gets while string.include?(trash_identity_string)
+  return string
 end
 
 def encoding_ip(ip)
-  @encoded_ip = 0
-  @arr_ip = ip.split(".")
-  @arr_ip.reverse!
-  @mult = 1
-  @arr_ip.each{ |e| @encoded_ip += e.to_i * @mult; @mult *= 256; }
-  return @encoded_ip
+  encoded_ip = 0
+  arr_ip = ip.split(".")
+  arr_ip.reverse!
+  mult = 1
+  arr_ip.each{ |e| encoded_ip += e.to_i * mult; mult *= 256; }
+  return encoded_ip
 end
 
 def decoding_string(string)
@@ -20,9 +20,9 @@ def decoding_string(string)
 end
 
 def compare_ip(ip, array)
-  @min_ip = array[0].to_i
-  @max_ip = array[1].to_i
-  if (@min_ip..@max_ip).include?(ip)
+  min_ip = array[0].to_i
+  max_ip = array[1].to_i
+  if (min_ip..max_ip).include?(ip)
     puts("This ip using in " + array[6].chomp + " group.")
     return true
   else
@@ -30,10 +30,10 @@ def compare_ip(ip, array)
   end
 end
 
-@ip = ARGV[0].to_s
-@encoded_ip = encoding_ip(@ip)
-@ip_archive = File.open("IpToCountry.csv", "r");
-@string = first_usefull_st(@ip_archive, "#")
-@string = @ip_archive.gets until ( @string == nil \
-                                  || compare_ip(@encoded_ip, decoding_string(@string)) )
-@ip_archive.close
+ip = ARGV[0].to_s
+encoded_ip = encoding_ip(ip)
+ip_archive = File.open("IpToCountry.csv", "r");
+string = first_usefull_st(ip_archive, "#")
+string = ip_archive.gets until ( string == nil \
+                                  || compare_ip(encoded_ip, decoding_string(string)) )
+ip_archive.close

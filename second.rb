5a43856feefdbcd3d38035f7f837a57f11295bdc
VadimGorbachevsky <shaersorrel@gmail.com>
2015-03-27 09:14:04 +0500

    Убрал промежуточный массив в second.rb dots_into_circle.

--- second.rb
+++ second.rb
@@ -1,11 +1,9 @@
 def dots_into_circle(dots, center, radius)
-  included_dots = []
   right_part = radius**2
-  dots.each do |e|
+  dots.partition do |e|
     left_part = (e.first + center.first)**2 + (e[1] + center[1])**2
-    (left_part > right_part) || included_dots << e
-  end
-  return included_dots
+    left_part <= right_part
+  end.first
 end
 
 dots = [[1,2],[2,3],[15,2],[7,0],[-4.5,4.5],[3.4,2.3],[1,1],[0,0]]
